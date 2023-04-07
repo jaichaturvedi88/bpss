a9ec5a1e484038658ecbb0e6ea3240d8d76c1a1a
Jai Chaturvedi <jai.chaturvedi88@gmail.com>
2023-04-07 09:54:33 +0530

    carousel poc done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule,
