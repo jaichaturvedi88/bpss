d081b22334689bfb8b4bba9657ee3156aa9a4806
sanchitking <sanchitparadhar2284@gmail.com>
2023-04-10 08:55:57 +0530

    created empty contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarouselComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
