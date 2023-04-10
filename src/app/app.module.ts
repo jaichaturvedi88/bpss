45072ca3e2f936d7a4c9010be20c80835349b61f
unknown <chaturvedikrishna812@gmail.com>
2023-04-10 14:23:59 +0530

    creating Gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarouselComponent,
+    GalleryComponent,
   ],
   imports: [
     BrowserModule,
