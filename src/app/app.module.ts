0a218caeb7e0aea3b0c65048cf6b64538691ac69
unknown <vivekthakur2128@gmail.com>
2023-04-10 15:10:29 +0530

    added trust members page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { TrustMembersComponent } from './trust-members/trust-members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarouselComponent,
+    TrustMembersComponent,
   ],
   imports: [
     BrowserModule,
