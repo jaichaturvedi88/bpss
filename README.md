77d85b58202caf5a3cbb7657342bad1c343a8ab4
Jai Chaturvedi <jai.chaturvedi88@gmail.com>
2023-04-27 01:42:34 +0530

    i18n working partially

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+
+
+# Jai
+## Commands and notes
+
+### localize notes
+
+ng build --localize  -> this will show 2 locales built
+
+To change development build locale -> make change in angular.json -> architect->build->options->localize
+
+### 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bpss';
+  title = 'Bhagwan Parshuram Sewa Samiti';
+
+  constructor(private titleService: Title){
+    // this.titleService.setTitle('bpss ng');
+    this.titleService.setTitle($localize`${this.title}`);
+  }
 }
