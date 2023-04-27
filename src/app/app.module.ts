ed332dfbe794e2acc9a2e9f8abb0cba0029fe182
Jai Chaturvedi <jai.chaturvedi88@gmail.com>
2023-04-27 17:12:45 +0530

    disabled service worker and i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,12 +26,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
+    // ServiceWorkerModule.register('ngsw-worker.js', {
+    //   enabled: !isDevMode(),
+    //   // Register the ServiceWorker as soon as the application is stable
+    //   // or after 30 seconds (whichever comes first).
+    //   registrationStrategy: 'registerWhenStable:30000'
+    // }),
   ],
   providers: [],
   bootstrap: [AppComponent]
