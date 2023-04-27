ff0f3f46cd921d696ca9506cfaea08c7ec213d89
Jai Chaturvedi <jai.chaturvedi88@gmail.com>
2023-04-27 19:06:15 +0530

    created a landing page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
+  public isAboutUsPage: boolean = false;
 
+  constructor(private router: Router) {
+    this.isAboutUsPage = this.router.url === '/about' ? true : false;
+
+  }
 }
